a3a53e423a46e068cfb015249eb5d2a7c0df3926
little-geng-cloud <1598384760@qq.com>
2024-05-05 21:58:52 +0800

    Hello

---
+++ com/gcs/test/Hello.java
@@ -0,0 +1,12 @@
+package com.gcs.test;
+
+/**
+ * @Author: Gcs
+ * @Date: 2024/5/5 21:57
+ * @Describe
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
