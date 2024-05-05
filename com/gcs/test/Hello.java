a3ff5c1bfc748cbb3e71261706c31f68649becaf
little-geng-cloud <1598384760@qq.com>
2024-05-05 22:04:41 +0800

    增加变量

--- com/gcs/test/Hello.java
+++ com/gcs/test/Hello.java
@@ -3,9 +3,10 @@ package com.gcs.test;
 /**
  * @Author: Gcs
  * @Date: 2024/5/5 21:57
- * @Describe
+ * @Describe try
  */
 public class Hello {
+    int a=0;
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
